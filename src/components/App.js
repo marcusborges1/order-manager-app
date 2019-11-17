99d5a3db8ea2e43d03eec7d11b3929e63b8549e6
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2019-11-17 01:36:54 -0200

    changes App to use BrowserRouter from react-router-dom.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import 'components/App.css';
 
 const App = () => (
-  <>
+  <Router>
     <header>
       <nav>
       </nav>
     </header>
     <main>
-      Hello Order Manager App
+      <Switch>
+        <Route path="/">
+          <h1>Products</h1>
+          <ul>
+            <li>Smartphone</li>
+            <li>Notebook</li>
+            <li>Gamer Keyboard</li>
+          </ul>
+        </Route>
+      </Switch>
     </main>
     <footer>
     </footer>
-  </>
+  </Router>
 );
 
 export default App;
